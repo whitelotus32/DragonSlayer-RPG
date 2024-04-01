0a5cb6b3c78a10c58500be7ef74ea199655c84cf
Nicholas Dixon <NickDixon32@proton.me>
2024-03-31 19:46:08 -0700

    Added innerText and onclicks to all buttons.

--- JS/script.js
+++ JS/script.js
@@ -23,7 +23,15 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function goStore () {
-    console.log("Going to store.");
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+
+    text.innerText = "You enter the store.";
 }
 
 function goCave() {
