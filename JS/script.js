64dfbbd1ccde573d6f1c0728d69c6aa2b96184b7
Nicholas Dixon <NickDixon32@proton.me>
2024-04-01 06:02:57 -0700

    Added properties and objects to the locations variable then consolidated my code.

--- JS/script.js
+++ JS/script.js
@@ -16,6 +16,20 @@ const goldText = document.querySelector("#goldText");
 const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const locations = [
+    {
+        name: "town square",
+        "button text": ["Go to store", "Go to cave", "Fight dragon"],
+        "button functions": [goStore, goCave, fightDragon],
+        text: "You are in the town square. You see a sign that says \"Store\"."
+    },
+    {
+        name: "store",
+        "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+        "button functions": [buyHealth, buyWeapon, goTown],
+        text: "You enter the store."
+    }
+];
 
 // initialize buttons
 button1.onclick = goStore;
