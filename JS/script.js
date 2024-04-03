8a37794ce635e9b3ea0afad7d242749976c5a2db
Nicholas Dixon <NickDixon32@proton.me>
2024-04-03 16:48:47 -0700

    Adjusted the if statement to check the weapons array and added an else statement.

--- JS/script.js
+++ JS/script.js
@@ -86,7 +86,7 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-    if (currentWeapon < 3) {
+    if (currentWeapon < weapons.length - 1) {
         if (gold >= 30) {
             gold -= 30;
             currentWeapon++;
@@ -98,6 +98,8 @@ function buyWeapon() {
         }   else {
             text.innerText = "You do not have enough gold to buy a weapon.";
         }
+    }   else {
+        text.innerText = "You already have the most powerful weapon!";
     }
 }
 
