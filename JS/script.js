d14f103607d140bb51d9f0da745a68cd1fef581e
Nicholas Dixon <NickDixon32@proton.me>
2024-04-03 17:11:11 -0700

    Built out the sellWeapon function.

--- JS/script.js
+++ JS/script.js
@@ -100,6 +100,20 @@ function buyWeapon() {
         }
     }   else {
         text.innerText = "You already have the most powerful weapon!";
+        button2.innerText = "Sell weapon for 15 gold";
+        button2.onclick = sellWeapon;
+    }
+}
+
+function sellWeapon() {
+    if (inventory.length > 1) {
+        gold += 15;
+        goldText.innerText = gold;
+        let currentWeapon = inventory.shift();
+        text.innerText = "You sold a " + currentWeapon + ".";
+        text.innerText += " In your inventory you have: " + inventory;
+    }   else {
+        text.innerText = "Don't sell your only weapon!";
     }
 }
 
