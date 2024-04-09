df2dd57508aa89fd423b2f3bd240e8d6b3759c4d
Nicholas Dixon <NickDixon32@proton.me>
2024-04-09 15:40:40 -0700

    Displayed the monster's stats element and updated the text for the current monster's name and health.

--- JS/script.js
+++ JS/script.js
@@ -156,6 +156,9 @@ function fightDragon() {
 function goFight() {
     update(locations[3]);
     monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsterHealth;
 }
 
 function attack() {
