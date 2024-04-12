e9dc752c8ebfc24742048391b2bf2cf01fb04c9f
Nicholas Dixon <NickDixon32@proton.me>
2024-04-11 19:51:11 -0700

    Updated the health variable to subtract from the monster's level from the player's current health.

--- JS/script.js
+++ JS/script.js
@@ -164,6 +164,7 @@ function goFight() {
 function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks.";
     text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+    health -= monsters[fighting].level; // Subtract the monster's level from health
 }
 
 function dodge() {
