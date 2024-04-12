7693a36fcc1149b019b60f9ec404521dd452373a
Nicholas Dixon <NickDixon32@proton.me>
2024-04-11 19:45:54 -0700

    Assigned a string in the attack function.

--- JS/script.js
+++ JS/script.js
@@ -163,6 +163,7 @@ function goFight() {
 
 function attack() {
     text.innerText = "The " + monsters[fighting].name + " attacks.";
+    text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
 }
 
 function dodge() {
